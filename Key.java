69b77ba31965e42dfb758c63f6e474c4d9fb2c27
Angelino dela Cruz <70660061+Angelino1059143@users.noreply.github.com>
2022-03-16 08:35:13 -0700

    Update Key.java

--- Key.java
+++ Key.java
@@ -1,116 +1,127 @@
 import java.awt.*;
-
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.sound.midi.*;
 import javax.swing.*;
 
-public class Key extends JButton {
-	
-	private Color backGround, foreGround;
-	private boolean isMinor;
-	private JFrame frame;
-	private  int WIDTH = 107;
-	private int startPos = 37;
-	
-	
-	
-	private String[] noteName =  {"C","D","E","F","G","A","B"};
-	
-	public Key(JFrame KeyBoard, int i)  {
-		this.frame = KeyBoard;
-		int noteRemainder = i % this.noteName.length;
-//		frame.setBounds(100, 100, 1244, 733);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.getContentPane().setLayout(null);
-		this.backGround = Color.white;
-		this.setBackground(backGround);
-		setTheFont(26);
-		this.setBounds(getLeft(i, false), 280, 97, 287);
-		this.frame.getContentPane().add(this);
-		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_C, 0), "C");
-		this.frame.setVisible(true);
-		this.addActionListener(e -> {
-			try {
-				selectionButtonPressed(e);
-			} catch (MidiUnavailableException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		});
-		
-		switch(noteRemainder) {
-			case 0:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 1:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 2:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 3:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 4:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 5:
-				this.setText(noteName[noteRemainder]);
-				break;
-			case 6:
-				this.setText(noteName[noteRemainder]);
-				break;
-		}
-	}
-//		Action aa = new AbstractAction()
-//		   {
-//				@Override
-//			  public void actionPerformed(ActionEvent e)
-//			   {
-//				  mc[5].noteOn(60, 600);                      
-//			   }
-//		   };			
-//		this.getActionMap().put("C", aa);
-//		this.addActionListener(aa);
-	
-	public int getLeft(int i, boolean isMinor) {
-		if(!isMinor) {
-			return this.startPos + i * this.WIDTH;
-			
-			
-			
-		} else if(isMinor) {
-			this.startPos = 79;
-			if(i % 5 > 1) {
-				return this.startPos + (i-1) * this.WIDTH + 177;
-			} else { 
-				return this.startPos + i * this.WIDTH;
-			}
-			
-		}
-		return 0;
-		
-	}
 
-	public void selectionButtonPressed(ActionEvent e) throws MidiUnavailableException {
-		Synthesizer synth = MidiSystem.getSynthesizer();
-		Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
-		MidiChannel[] mc = synth.getChannels();
-		synth.open();
-		synth.loadInstrument(instr[90]);
-		mc[5].noteOn(60,600);
-		
-		
-	}
-	
-	
-	public void setWIDTH(int x) {
-		this.WIDTH = x;
-	}
+public class Key extends JButton  {
 
-	public void setTheFont(int x) {
-		this.setFont(new Font("Tahoma", Font.PLAIN, x));
-	}
+  private Color backGround, foreGround;
+  private boolean isMinor;
+  private JFrame frame;
+  private int WIDTH = 107;
+  private int startPos = 37;
+  private String testName = "Brad";
+  private int i;
+  private int noteNum = 60;
+  
+
+  private String[] noteName = { "C", "D", "E", "F", "G", "A", "B" };
+  
+
+  public Key(JFrame KeyBoard, int i) {
+	this.i = i;
+    this.frame = KeyBoard;
+    int noteRemainder = i % this.noteName.length;
+    // frame.setBounds(100, 100, 1244, 733);
+    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.getContentPane().setLayout(null);
+    this.backGround = Color.white;
+    this.setBackground(backGround);
+    setTheFont(26);
+    this.setBounds(getLeft(i, false), 280, 97, 287);
+    this.frame.getContentPane().add(this);
+    this.addActionListener(e -> selectionButtonPressed(e));
+    this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_C, 0), "C");
+    this.frame.setVisible(true);
+
+    switch (noteRemainder) {
+      case 0:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 1:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 2:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 3:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 4:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 5:
+        this.setText(noteName[noteRemainder]);
+        break;
+      case 6:
+        this.setText(noteName[noteRemainder]);
+        break;
+    }
+  }
+  public void selectionButtonPressed(ActionEvent e){
+    System.out.println("Note Number: " + (this.i + 60));
+    // Setting up the instrument..
+    // TODO: this should be moved back to the piano at some point...
+    try {
+    Synthesizer synth = MidiSystem.getSynthesizer();
+    Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
+    MidiChannel[] mc = synth.getChannels();
+		
+      synth.open();
+    
+    
+    synth.loadInstrument(instr[90]);
+    
+    System.out.println(e.paramString());
+    
+    mc[5].noteOn(60 + this.i,600);
+  }
+    catch(Exception ex){
+      
+    }
+  }
+
+
+  
+  // Action aa = new AbstractAction()
+  // {
+  // @Override
+  // public void actionPerformed(ActionEvent e)
+  // {
+  // mc[5].noteOn(60, 600);
+  // }
+  // };
+  // this.getActionMap().put("C", aa);
+  // this.addActionListener(aa);
+
+  public int getLeft(int i, boolean isMinor) {
+    if (!isMinor) {
+      return this.startPos + i * this.WIDTH;
+
+    } else if (isMinor) {
+      this.startPos = 79;
+      if (i % 5 > 1) {
+        return this.startPos + (i - 1) * this.WIDTH + 177;
+      } else {
+        return this.startPos + i * this.WIDTH;
+      }
+
+    }
+    return 0;
+
+  }
+
+  public void setWIDTH(int x) {
+    this.WIDTH = x;
+  }
+
+  public void setTheFont(int x) {
+    this.setFont(new Font("Tahoma", Font.PLAIN, x));
+  }
+  
+
 }
