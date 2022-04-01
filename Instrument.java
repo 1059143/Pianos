66995b9d23ad3f3fb26987f771abac48e6cda9fe
Angelino Dela Cruz <1059143@apps.nsd.org>
2022-04-01 09:45:48 -0700

    instrument fixes

--- Instrument.java
+++ Instrument.java
@@ -11,15 +11,23 @@ import java.awt.*;
 
 
 
-public class Instrument {
+public class Instrument extends JButton {
 	
+	private int instru;
 	
-	public Instrument() {
+	private JFrame frame;
+	
+	public Instrument(JFrame KeyBoard) {
 		
+		this.frame = KeyBoard;
 		String x = JOptionPane.showInputDialog("Pick a number from 0 to 128");
+		this.instru = Integer.parseInt(x);
 		
 		
 	}
 	
 	
+	private int getInstr() {
+		return this.instru;
+	}
 }
